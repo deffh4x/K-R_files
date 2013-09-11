9175067e8c60dc594228aae55f1b7eecc70e6a9c
deffh4x <deffh4x@gmail.com>
2013-09-10 22:49:28 -0400

    move parsing code to function

--- daymonth.c
+++ daymonth.c
@@ -13,41 +13,48 @@ char daytab[2][13]= {
 /*function declarations*/
 int dayOfYear(int year, int month, int day);
 void monthDay(int year, int day, int *pmonth, int *pday);
-void parseDate(char *dateString, int stringLength);
+void parseDate(char *dateString, size_t *stringLength);
 
 int main() 
 {
-    int *pmonth, *pyear,*pday, len, userDate[3];
-    int i=0;
+    int *pmonth, *pyear,*pday; 
     size_t maxLen = MAXLEN;
     char *currLine=malloc(maxLen);
-    char *bufferStart=currLine;
-    char *nextField;
 
     printf("Please enter either the year,month,day or just year,day:\n");
-    while((len=getline(&currLine, &maxLen, stdin)) && len != -1) {
-        while (((nextField = strchr(currLine, ',')) || (nextField = strchr(currLine, '\n'))) && i < 3) 
-        {
-            *(nextField++) = '\0';
-            if(userDate[i++] = atoi(currLine))
-                currLine += (nextField - currLine);
-            else 
-            {
-                printf("Invalid value entered\n");
-                break;
-            }
-        }
-        if (i > 1 && i < 4) {
-            if (i == 2)
-                monthDay(userDate[0],userDate[1],pmonth,pday);
-            if (i == 3)
-                dayOfYear(userDate[0],userDate[1],userDate[2]);
-        }
-        else
-            printf("Invalid number of arguments\n");
-
-        currLine = bufferStart; //reset buffer
-        i=0; //reset date
-    }
+    parseDate(currLine, &maxLen);
     return 0;
+}
+
+void parseDate(char *dateString, size_t *stringLength)
+{
+    int i=0;
+	int len,userDate[3];
+    char *bufferStart=dateString;
+    char *nextField;
+	
+	while((len=getline(&dateString, &stringLength, stdin)) && len != -1) {
+		while (((nextField = strchr(dateString, ',')) || (nextField = strchr(dateString, '\n'))) && i < 3) 
+		{
+			*(nextField++) = '\0';
+			if(userDate[i++] = atoi(dateString))
+				dateString += (nextField - dateString);
+			else 
+			{
+				printf("Invalid value entered\n");
+				break;
+			}
+		}
+		/*if (i > 1 && i < 4) {
+			if (i == 2)
+				monthDay(userDate[0],userDate[1],pmonth,pday);
+			if (i == 3)
+				dayOfYear(userDate[0],userDate[1],userDate[2]);
+		}
+		else
+			printf("Invalid number of arguments\n");*/
+
+		dateString = bufferStart; //reset buffer
+		i=0; //reset date
+	}
 }
